ca3490a8bc7c1686de9b8ef58444dd2b5971315d
WhiteCells <xuan2692659908@foxmail.com>
2024-08-21 19:43:29 +0800

    init thread pool

---
+++ README.md
@@ -0,0 +1,17 @@
+### C++14 Thread Pool
+
+```sh
+cmake -B build
+cmake --build build
+./build/thread_pool
+```
+
+usage:
+
+```cpp
+auto &pool = ThreadPool::getInstance();
+auto res = pool.addTask([](int num) -> int {
+    return num + 10;
+}, 20);
+std::cout << res.get() << std::endl;
+```
---
+++ example.cpp
@@ -0,0 +1,17 @@
+#include "thread_pool.hpp"
+
+int main(int argc, char *argv[]) {
+    std::vector<std::future<int>> results;
+    ThreadPool &pool = ThreadPool::getInstance();
+    for (int i = 0; i < 20; ++i) {
+        results.emplace_back(pool.addTask([](int num) -> int {
+            std::cout << "thread: " << num << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            return num * num;
+        }, i));
+    }
+    for (auto &res : results) {
+        std::cout << "ret: " << res.get() << std::endl;
+    }
+    return 0;
+}
