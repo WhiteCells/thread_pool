b32e528c9a095ad511b48d328b8f494d0c09e8a5
WhiteCells <xuan2692659908@foxmail.com>
2024-08-23 12:50:51 +0800

    updated .clang-format

--- example.cpp
+++ example.cpp
@@ -5,11 +5,13 @@ int main(int argc, char *argv[]) {
     std::vector<std::future<int>> results;
     ThreadPool &pool = ThreadPool::getInstance();
     for (int i = 0; i < 20; ++i) {
-        results.emplace_back(pool.addTask([](int num) -> int {
-            std::cout << "thread: " << num << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            return num * num;
-        }, i));
+        results.emplace_back(pool.addTask(
+            [](int num) -> int {
+                std::cout << "thread: " << num << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                return num * num;
+            },
+            i));
     }
     for (auto &res : results) {
         std::cout << "ret: " << res.get() << std::endl;
